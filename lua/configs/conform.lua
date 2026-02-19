677f13444568398b82afd973be99aedb1b95f45c
Woody <mgudwin@gmail.com>
2026-02-18 19:56:08 -0500

    nvchad

---
+++ lua/configs/conform.lua
@@ -0,0 +1,15 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    markdown = { "prettier" },
+    python = { "black" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
