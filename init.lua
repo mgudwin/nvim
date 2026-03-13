19927607a914f1e8f0294275a9ddbac034391d18
Woody <mgudwin@gmail.com>
2026-03-13 18:00:23 -0400

    readme

--- init.lua
+++ init.lua
@@ -776,7 +776,7 @@ require('lazy').setup({
     },
   },
   {
-    'catppuccin-nvim/nvim',
+    'catppuccin/nvim',
     lazy = false,
     priority = 1000,
     config = function() vim.cmd.colorscheme 'catppuccin-mocha' end,
